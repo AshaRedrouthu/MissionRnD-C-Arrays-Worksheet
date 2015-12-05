60f9f55a9c9923869db45fd438a769f4047c76e4
AshaRedrouthu <ashajyothiredrouthu@gmail.com>
2015-12-05 18:25:36 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -15,5 +15,23 @@ NOTES: Don't create new array, try to change the input array.
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
+	if (Arr == NULL || len <= 0)
+		return NULL;
+	for (int i = 0; i < len; i++){
+		for (int j = 0; j < len; j++){
+			if (i == j)
+				continue;
+			else if (*(Arr + i) == *(Arr + j)){
+				int k = j;
+				len--;
+				while (k < len){
+					*(Arr + k) = *(Arr + k + 1);
+					k++;
+				}
+				j = 0;
+			}
+
+		}
+	}
 	return NULL;
 }
